22141f0e81d19e26de6b5663f05214deff5c5d23
Your Name <you@example.com>
2024-08-22 02:35:21 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./resources/css/app.css">
+  <style>
+    body { background-color: red; }
+  </style>
+</head>
+<bod class="bg-red-00">
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</body>
+</html>
